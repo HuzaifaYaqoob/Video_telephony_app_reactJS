6c3dd8bd72cb66a724f708074ba18d1f50e2f49a
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-07-30 19:38:20 +0500

    header added

---
+++ pentutor_video_telephony/src/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import Cookies from "js-cookie"
+import { connect } from "react-redux"
+
+
+
+const Header = (props) => {
+    return (
+        <>
+            <div className="w-full">
+                <div className="py-3 px-3 max-w-[1300px] gap-5 w-full mx-auto flex items-center justify-between">
+                    <div>
+                        <img src={`${process.env.PUBLIC_URL}/images/logo.webp`} className='w-[260px]' alt="" />
+                    </div>
+                    {
+                        props.user.profile?.user && Cookies.get('auth_token') &&
+                        <div className="flex items-center gap-4">
+                            <div className="bg-gray-900 text-white px-3 py-2 rounded-md cursor-pointer hover:shadow-lg">
+                                Logout
+                            </div>
+                            <div className="w-[55px] h-[55px] bg-gray-200 rounded-full flex items-center justify-center capitalize text-[25px]">
+                                {props.user.profile?.user.username[0]}
+                            </div>
+                        </div>
+                    }
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+const mapState = state => {
+    return state
+}
+
+export default connect(mapState, null)(Header)
--- pentutor_video_telephony/src/Pages/Homepage/index.jsx
+++ pentutor_video_telephony/src/Pages/Homepage/index.jsx
@@ -5,6 +5,7 @@ import { get_user } from "../../redux/actions/Auth";
 import Cookies from 'js-cookie'
 import { createNewVideoMeeting } from "../../redux/actions/Video";
 import { Triangle } from "react-loader-spinner";
+import Header from "../../Components/Header/Header";
 
 
 const Homepage = (props) => {
@@ -67,6 +68,7 @@ const Homepage = (props) => {
     }, [])
     return (
         <>
+            <Header />
             <div className="flex items-center p-3 sm:p-0 justify-center md:justify-between flex-col md:flex-row h-screen max-w-5xl mx-auto gap-10">
                 <div className="md:flex-1">
                     <h3 className="text-4xl text-center mb-2 text-gray-900">Premium video meeting now free for everyone</h3>
