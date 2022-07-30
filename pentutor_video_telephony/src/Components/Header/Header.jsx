5ef7ed0af5d28171c88f7991d173bd447bbf6506
HuzaifaYaqoob <zeevhyorg@gmail.com>
2022-07-30 19:41:12 +0500

    logout user functionality

--- pentutor_video_telephony/src/Components/Header/Header.jsx
+++ pentutor_video_telephony/src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import Cookies from "js-cookie"
 import { connect } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
 
 
 const Header = (props) => {
+    const navigate = useNavigate()
     return (
         <>
             <div className="w-full">
@@ -14,7 +16,13 @@ const Header = (props) => {
                     {
                         props.user.profile?.user && Cookies.get('auth_token') &&
                         <div className="flex items-center gap-4">
-                            <div className="bg-gray-900 text-white px-3 py-2 rounded-md cursor-pointer hover:shadow-lg">
+                            <div
+                                className="bg-gray-900 text-white px-3 py-2 rounded-md cursor-pointer hover:shadow-lg"
+                                onClick={() => {
+                                    Cookies.remove('auth_token')
+                                    navigate(0)
+                                }}
+                            >
                                 Logout
                             </div>
                             <div className="w-[55px] h-[55px] bg-gray-200 rounded-full flex items-center justify-center capitalize text-[25px]">
