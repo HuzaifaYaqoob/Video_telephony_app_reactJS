f5c6899b94168fa0608afa26ddd6840592b66756
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-10 13:10:13 +0500

    Auto commit at 2024-06-10 13:10:13

--- src/Components/VideoStream/VideoBlock.jsx
+++ src/Components/VideoStream/VideoBlock.jsx
@@ -16,7 +16,7 @@ const UserVideoBlock = ({user, mediaStream}) =>{
     }, [rm_vid.current])
     return (
         <>
-            <div className="border relative rounded-3xl overflow-hidden h-[250px] min-w-[300px] flex-1 flex items-center justify-center border-gray-300">
+            <div className="mb-4 sm:mb-0 border relative rounded-3xl overflow-hidden h-[250px] min-w-[300px] flex-1 flex items-center justify-center border-gray-300">
                 <p className={`absolute text-white ${is_vid_active ? 'bottom-0 left-0 bg-black px-3 py-1' : ''}`}>{user.first_name} {user.last_name}</p>
                 <video ref={rm_vid} autoPlay muted className="w-auto max-w-full max-h-full h-auto"></video>
             </div>
