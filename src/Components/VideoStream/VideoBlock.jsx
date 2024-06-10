9143e62b707b9138fa95d21e8f1298a29c396437
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-10 13:11:44 +0500

    Auto commit at 2024-06-10 13:11:44

--- src/Components/VideoStream/VideoBlock.jsx
+++ src/Components/VideoStream/VideoBlock.jsx
@@ -6,6 +6,8 @@ import { ToggleVideoMode } from "../../redux/actions/Video"
 const UserVideoBlock = ({user, mediaStream}) =>{
     const rm_vid = useRef(null)
     const is_vid_active = mediaStream?.getVideoTracks()[0].enabled
+    const is_audio_active = mediaStream?.getAudioTracks()[0].enabled
+    console.log('audio, ', is_audio_active)
 
 
     useEffect(() => {
